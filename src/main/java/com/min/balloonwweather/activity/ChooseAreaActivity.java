eedccc768695ff85a06ff6f7d0d170d9f1c65d6f
mic <675660665@qq.com>
2015-12-23 16:05:32 +0800

    去除了城市选择界面的floatingbutton

--- src/main/java/com/min/balloonwweather/activity/ChooseAreaActivity.java
+++ src/main/java/com/min/balloonwweather/activity/ChooseAreaActivity.java
@@ -107,14 +107,6 @@ public class ChooseAreaActivity extends AppCompatActivity {
         });
         queryProvinces();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
